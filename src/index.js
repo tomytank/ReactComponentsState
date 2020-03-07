6c273b3b457a730f8a4b36c861eb573f6a6b91f9
tomytank <github@iamtom.com>
2020-03-07 12:20:29 +0100

    Added additional notes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState } from "react";
 import "./styles.css";
 //import App from "./App";
 
@@ -12,18 +13,39 @@ const Counter = () => {
   //);
   //return counterContainer;
   // document.createElement("div");
+  //{currentValueOfTheState, functionThatAllowsUsToChangeState}
+  const [total, setTotal] = useState(0);//this is a hook!
 
   return (
     <div className="counter">
       <h1>JS Counter</h1>
       <p className="click_desc">
-        The total is currently <span>0</span>
+        The total is currently <span>{total}</span>
       </p>
       <div className="button_container">
-        <button>Add 1</button>
-        <button>Multiply by 5</button>
-        <button>Reset</button>
+        <button
+          onClick={() => {
+            setTotal(total + 1);
+          }}
+        >
+          Add 1
+        </button>
+        <button
+          onClick={() => {
+            setTotal(total * 5);
+          }}
+        >
+          Multiply by 5
+        </button>
+        <button
+          onClick={() => {
+            setTotal(0);
+          }}
+        >
+          Reset
+        </button>
       </div>
+      <h1 id="big_total">{total}</h1>
     </div>
   );
 };
